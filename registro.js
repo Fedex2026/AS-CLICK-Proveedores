e9b30abcb7ae4ced560e11d6523ba285225e229f
Fedex2026 <gruasmetro@gmail.com>
2026-07-30 17:06:07 -0600

    Create registro.js

---
+++ registro.js
@@ -0,0 +1,9 @@
+const formulario = document.getElementById("registerForm");
+
+formulario.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    alert("Registro funcionando correctamente.");
+
+});
