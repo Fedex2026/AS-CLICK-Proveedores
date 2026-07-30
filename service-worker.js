21d66337085e66d80786c294182dbf4daa19be85
Fedex2026 <gruasmetro@gmail.com>
2026-07-30 16:53:53 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="as-click-proveedores-v1";
+const FILES=["./","./login.html","./index.html","./styles.css","./login.js","./app.js","./firebase-config.js","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
