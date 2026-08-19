e9bdb74e3da98ed30e3a2b55588348d39f5699f6
Fedex2026 <gruasmetro@gmail.com>
2026-08-19 00:59:07 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,1 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js"
-);
 
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js"
-);
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDeuQxzRhfVB9rXKD1pnOrNMXbrZnDj4UU",
-  authDomain: "as-clicl-mexico.firebaseapp.com",
-  databaseURL: "https://as-clicl-mexico-default-rtdb.firebaseio.com",
-  projectId: "as-clicl-mexico",
-  storageBucket: "as-clicl-mexico.firebasestorage.app",
-  messagingSenderId: "908429271001",
-  appId: "1:908429271001:web:40149a91fb2eef3ab4c3c8"
-});
-
-const messaging = firebase.messaging();
